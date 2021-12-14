2d4a5fa00b5e0e66dfe5406345126162dc6f5806
Anton Shlain <ash.glc@gmail.com>
2021-12-15 01:21:10 +0600

    14.12.2021

--- src/main/java/ed/inno/javajunior/booklab/configs/MvcConfig.java
+++ src/main/java/ed/inno/javajunior/booklab/configs/MvcConfig.java
@@ -1,15 +1,9 @@
 package ed.inno.javajunior.booklab.configs;
 
-import org.apache.catalina.startup.Tomcat;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.io.File;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -19,5 +13,4 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
     }
-
 }
